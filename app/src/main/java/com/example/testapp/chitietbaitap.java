60a7fd7b7f690dda0d5b7a4d5abc2e4ed75295cc
DouDou <tungdo1806@gmail.com>
2023-08-09 23:42:12 +0700

    xong chức năng cơ bản , còn tình tổng tất cả các bài tập chưa tìm ra cách

--- app/src/main/java/com/example/testapp/chitietbaitap.java
+++ app/src/main/java/com/example/testapp/chitietbaitap.java
@@ -58,9 +58,6 @@ public class chitietbaitap extends AppCompatActivity {
         //====================================
         if (savedInstanceState != null) {
 
-            // Get the previous state of the stopwatch
-            // if the activity has been
-            // destroyed and recreated.
             seconds
                     = savedInstanceState
                     .getInt("seconds");
@@ -102,18 +99,10 @@ public class chitietbaitap extends AppCompatActivity {
         }
     }
 
-    // Start the stopwatch running
-    // when the Start button is clicked.
-    // Below method gets called
-    // when the Start button is clicked.
     public void onClickStart(View view) {
         running = true;
     }
 
-    // Stop the stopwatch running
-    // when the Stop button is clicked.
-    // Below method gets called
-    // when the Stop button is clicked.
     public void onClickStop(View view) {
         running = false;
         DecimalFormat df = new DecimalFormat("#.#");
@@ -129,27 +118,12 @@ public class chitietbaitap extends AppCompatActivity {
         seconds = 0;
     }
 
-    // Sets the NUmber of seconds on the timer.
-    // The runTimer() method uses a Handler
-    // to increment the seconds and
-    // update the text view.
     private void runTimer() {
 
-        // Get the text view.
-        final TextView timeView
-                = (TextView) findViewById(
-                R.id.time_view);
+        final TextView timeView = (TextView) findViewById(R.id.time_view);
 
-        // Creates a new Handler
-        final Handler handler
-                = new Handler();
+        final Handler handler = new Handler();
 
-        // Call the post() method,
-        // passing in a new Runnable.
-        // The post() method processes
-        // code without a delay,
-        // so the code in the Runnable
-        // will run almost immediately.
         handler.post(new Runnable() {
             @Override
 
@@ -158,25 +132,13 @@ public class chitietbaitap extends AppCompatActivity {
                 int minutes = (seconds % 3600) / 60;
                 int secs = seconds % 60;
 
-                // Format the seconds into hours, minutes,
-                // and seconds.
-                String time
-                        = String
-                        .format(Locale.getDefault(),
-                                "%d:%02d:%02d", hours,
-                                minutes, secs);
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
 
-                // Set the text view text.
                 timeView.setText(time);
 
-                // If running is true, increment the
-                // seconds variable.
                 if (running) {
                     seconds++;
                 }
-
-                // Post the code again
-                // with a delay of 1 second.
                 handler.postDelayed(this, 1000);
             }
         });
